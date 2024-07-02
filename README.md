54dd23c317a6a124cb849beef738d3358ef67c5e
Saifur Rahman Shihab <thecodemaster26@gmail.com>
2024-07-02 18:09:16 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# geek-station
+ A blog that caters to all things geeky, from movies and TV shows to video games and technology
---
+++ dummy.php
@@ -0,0 +1,36 @@
+<?php
+    include 'includes/header.php';
+?>
+
+<div class="page-content">
+    <div class="container-fluid">
+        
+        <!-- start page title -->
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                    <h4 class="mb-sm-0">Dashboard</h4>
+
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="<?php echo $_SERVER['PHP_SELF']?>">Upcube</a></li>
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- end page title -->
+
+        <!-- Code Here -->
+        <!-- Code Here -->
+
+    </div>
+    
+</div>
+<!-- End Page-content -->
+
+<?php
+    include 'includes/footer.php';
+?>
